3c3ad2683110c919756eb30ffee9c3430289f2bf
Joao-Vitor7 <joaovitormendo3@gmail.com>
2026-02-19 16:52:12 -0300

    Finalização.

--- Jogo2_Ovos.py
+++ Jogo2_Ovos.py
@@ -1,27 +1,33 @@
-<<<<<<< Updated upstream
-tentativas = 1
+import os
+os.system("cls")
+print("""
+█▀█ ▄▀█ █▀█ █▄▀ █░█ █▀█ █▀▀ █ █▀█ █▀█   █▀▀ █▀▀ █▀▀ █▀     ▄██▄
+█▀▀ █▀█ █▀▄ █░█ █▄█ █▀▄ ██▄ █ █▀▄ █▄█   ██▄ █▄█ █▄█ ▄█    ██████       
+                                                          ▀████▀""")
+comecar = input("Aperte enter para começar: ")
+os.system("cls")
+comecar = "enter"
+tentativas = 0
 condicao = True
 while condicao:
     print("Tente adivinhar em que andar o ovo quebra. Você terá duas tentativas, boa sorte.")
     usuario = int(input("Digite um andar de 1 a 100 para arremessar seu ovo: "))
-    if usuario >= 15 and usuario <= 50:
-        print("Ovo sobreviveu")
+    if usuario >= 45 and usuario <= 67:
+        print("Ovo sobreviveu.")
         break
     elif usuario == 0:
-        print("Engana outro, mas não eu.")
+        os.system("cls")
+        print("Tu é beta! Hahaha")
         continue
-    else:
-        print("Wasted.")
+    elif tentativas<=1:
+        print("Quebrou o ovo")
+        os.system("cls")
     if tentativas == 2:
         condicao = False
     tentativas +=1
-=======
-import os
-os.system("cls")
-print("""
-█▀█ ▄▀█ █▀█ █▄▀ █░█ █▀█ █▀▀ █ █▀█ █▀█   █▀▀ █▀▀ █▀▀ █▀
-█▀▀ █▀█ █▀▄ █░█ █▄█ █▀▄ ██▄ █ █▀▄ █▄█   ██▄ █▄█ █▄█ ▄█""")
-comecar = input("Aperte enter para começar: ")
-os.system("cls")
-comecar = "enter"
->>>>>>> Stashed changes
+    if usuario < 45:
+        print("É um número maior.")
+    elif usuario > 67:
+        print("É um número menor.")
+    if tentativas == 2:
+        print("CHANCE DECISIVA!")
