690f0ec541f968b9f60856ba08fc3971103b3252
LuisMiguel <miguelpkxd3@gmail.com>
2026-02-20 12:53:27 -0300

    Update_final--Luis
    Modificações finais.

--- Jogo2_Ovos.py
+++ Jogo2_Ovos.py
@@ -1,33 +1,56 @@
 import os
-os.system("cls")
-print("""
-█▀█ ▄▀█ █▀█ █▄▀ █░█ █▀█ █▀▀ █ █▀█ █▀█   █▀▀ █▀▀ █▀▀ █▀     ▄██▄
+
+while True:
+    os.system("cls")
+
+    print("""
+█▀█ ▄▀█ █▀█ █▄▀ █░█ █▀█ █▀▀ █ █▀█ █▀█   █▀▀ █▀▀ █▀▀ █▀     ▄██▄
 █▀▀ █▀█ █▀▄ █░█ █▄█ █▀▄ ██▄ █ █▀▄ █▄█   ██▄ █▄█ █▄█ ▄█    ██████       
                                                           ▀████▀""")
-comecar = input("Aperte enter para começar: ")
-os.system("cls")
-comecar = "enter"
-print("Tente adivinhar em que andar o ovo quebra. Você terá duas tentativas, boa sorte.")
-tentativas = 0
-condicao = True
-while condicao:
-    usuario = int(input("Digite um andar de 1 a 100 para arremessar seu ovo: "))
-    if usuario >= 45 and usuario <= 67:
-        print("Ovo sobreviveu.")
+
+    input("Aperte enter para começar: ")
+    os.system("cls")
+
+    print("Tente adivinhar em que andar o ovo sobrevive.")
+    print("Você terá duas tentativas, boa sorte.\n")
+
+    tentativas = 0
+    condicao = True
+
+    while condicao:
+        try:
+            usuario = int(input("Digite um andar de 1 a 100 para arremessar seu ovo: "))
+            os.system("cls")
+        except ValueError:
+            print("Digite apenas números.")
+            continue
+
+        if usuario == 0:
+            os.system("cls")
+            print("Tu é beta! Hahaha")
+            continue
+
+        tentativas += 1
+
+        if 45 <= usuario <= 67:
+            print("Ovo sobreviveu! Você venceu!")
+            break
+        else:
+            print("Quebrou o ovo")
+
+            if usuario < 45:
+                print("É um número maior.")
+            elif usuario > 67:
+                print("É um número menor.")
+
+        if tentativas == 2:
+            print("CHANCE DECISIVA!")
+
+        if tentativas == 3:
+            print("Você perdeu.")
+            break
+
+    reiniciar = input("\nAperte ENTER para reiniciar ou digite 'sair' para finalizar: ").lower()
+
+    if reiniciar == "sair":
         break
-    elif usuario == 0:
-        os.system("cls")
-        print("Tu é beta! Hahaha")
-        continue
-    elif tentativas<=1:
-        print("Quebrou o ovo")
-        os.system("cls")
-    if tentativas == 2:
-        condicao = False
-    tentativas +=1
-    if usuario < 45:
-        print("É um número maior.")
-    elif usuario > 67:
-        print("É um número menor.")
-    if tentativas == 2:
-        print("CHANCE DECISIVA!")
